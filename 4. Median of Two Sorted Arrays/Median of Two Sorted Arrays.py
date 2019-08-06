09966a97d55f17370f69f434c43ab437a99f6d7c
Chuyang Zhang <zhangchuyang2010@hotmail.com>
2019-08-06 15:46:14 -0700

    Delete Median of Two Sorted Arrays.py

--- 4. Median of Two Sorted Arrays/Median of Two Sorted Arrays.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        nums3 = nums1 + nums2
-        nums3 = sorted(nums3)
-        print(nums3)
-        if len(nums3) % 2 == 0:
-            return (nums3[len(nums3) / 2 - 1] + nums3[len(nums3) / 2]) / 2.0
-        else:
-            return float(nums3[int(math.ceil(len(nums3) / 2))])
